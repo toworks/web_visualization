c17c182217b1c7fab77781d004bc6489764f1a25
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2016-08-10 16:02:49 +0300

    new name table

--- sql/select_tree_menu.sql
+++ sql/select_tree_menu.sql
@@ -1,17 +1,19 @@
+
 WITH tree (id, parent, level, name, type, link, rn) as 
 (
    SELECT id, parent, 0 as level, name, type, link,
        convert(varchar(max),right(row_number() over (order by id),10)) rn
-   FROM [KRR-PA-DEV-Development]..menu
+   FROM [KRR-PA-GLB-SERVICE]..visualization_menu
    WHERE parent is null
 
    UNION ALL
 
    SELECT c2.id, c2.parent, tree.level + 1, c2.name, c2.type, c2.link,
        rn + '/' + convert(varchar(max),right(row_number() over (order by tree.id),10))
-   FROM [KRR-PA-DEV-Development]..menu c2 
+   FROM [KRR-PA-GLB-SERVICE]..visualization_menu c2 
      INNER JOIN tree ON tree.id = c2.parent
 )
 SELECT *
 FROM tree
 order by RN
+
