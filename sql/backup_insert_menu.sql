84beac1adfd8ea870c5f5d07193dfbe5ed1eae3d
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2017-03-28 11:33:02 +0300

    update backup script

--- sql/backup_insert_menu.sql
+++ sql/backup_insert_menu.sql
@@ -1,10 +1,16 @@
 select
 
 N'insert into [KRR-PA-GLB-SERVICE].[dbo].visualization_menu 
-( [id], [name], [link], [type], [parent] )
+( [id], [name], [link], [type], [parent], [sortid] )
 values( '+
-cast(id as nvarchar(max)) + ', N'''+name+''', N'''+link+''', N'''+type+''', '+isnull(cast(parent as nvarchar(max)), 'NULL')+
+	cast(id as nvarchar(max)) + ', N'''+
+	name+''', N'''+
+	link+''', N'''+
+	type+''', '+
+	isnull(cast(parent as nvarchar(max)), 'NULL')+', '+
+	isnull(cast(sortid as nvarchar(max)), 'NULL')+
 N')'
 
 from [KRR-PA-GLB-SERVICE].[dbo].visualization_menu
 where name not like '%older%' and name not like '%link%' and name not like '%root%'
+
